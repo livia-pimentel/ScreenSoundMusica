01c853ac6b14b4b5ce18b5275cdc62ee11941317
livia-pimentel <livia_macedo89@yahoo.com.br>
2026-01-21 19:28:12 -0300

    Estrutura dos métodos

--- src/main/java/br/com/alura/ScreenSoundMusica/principal/Principal.java
+++ src/main/java/br/com/alura/ScreenSoundMusica/principal/Principal.java
@@ -1,6 +1,10 @@
 package br.com.alura.ScreenSoundMusica.principal;
 
+import java.util.Scanner;
+
 public class Principal {
+    Scanner leitura = new Scanner(System.in);
+
     public void exibeMenu() {
         var opcao = -1;
 
@@ -16,6 +20,53 @@ public class Principal {
                     
                     9 - Sair
                     """;
+
+            System.out.println(menu);
+            opcao = leitura.nextInt();
+            leitura.nextLine();
+
+            switch (opcao) {
+                case 1:
+                    cadastrarArtistas();
+                    break;
+                case 2:
+                    cadastrarMusicas();
+                    break;
+                case 3:
+                    listarMusicas();
+                    break;
+                case 4:
+                    buscarMusicaPorArtista();
+                    break;
+                case 5:
+                    pesquisarDadosDoArtista();
+                    break;
+                case 9:
+                    System.out.println("Encerrando a aplicação!");
+                    break;
+                default:
+                    System.out.println("Opção inválida!");
+            }
         }
+    }
+
+    private void cadastrarArtistas() {
+
+    }
+
+    private void cadastrarMusicas() {
+
+    }
+
+    private void listarMusicas() {
+
+    }
+
+    private void buscarMusicaPorArtista() {
+
+    }
+
+    private void pesquisarDadosDoArtista() {
+
     }
 }
