b021c27c0cce3298a1f436fbe4831e6d1616bfad
livia-pimentel <livia_macedo89@yahoo.com.br>
2026-01-21 20:47:57 -0300

    Add getters e setters da classe artista

--- src/main/java/br/com/alura/ScreenSoundMusica/model/Artista.java
+++ src/main/java/br/com/alura/ScreenSoundMusica/model/Artista.java
@@ -3,6 +3,7 @@ package br.com.alura.ScreenSoundMusica.model;
 import jakarta.persistence.*;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "artistas")
@@ -18,4 +19,44 @@ public class Artista {
 
     private List<Musica> musicas = new ArrayList<>();
 
+    // Getters e Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public TipoArtista getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoArtista tipo) {
+        this.tipo = tipo;
+    }
+
+    public List<Musica> getMusicas() {
+        return musicas;
+    }
+
+    public void setMusicas(List<Musica> musicas) {
+        this.musicas = musicas;
+    }
+
+    @Override
+    public String toString() {
+        return "Artista='" + nome + '\'' +
+                ", tipo=" + tipo +
+                ", musicas=" + musicas;
+    }
 }
